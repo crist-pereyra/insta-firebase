ba45dfdd172fad64ff3c93ee577218f0b966f027
root <root@DESKTOP-LQPARAO.localdomain>
2021-08-03 19:43:45 -0400

    Create backend with Firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+  // Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDe0EyDRSWXM5_g74WGU840xYsrTWo0v3A",
+    authDomain: "cristpreyra-firegram.firebaseapp.com",
+    projectId: "cristpreyra-firegram",
+    storageBucket: "cristpreyra-firegram.appspot.com",
+    messagingSenderId: "629172599713",
+    appId: "1:629172599713:web:c1bfefdb4bad882e98e9e0"
+};
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
