f4f71145260ccae7a3193d788cf6aa5ab4a28cf7
Cristian Pereyra <cp.crist.pereyra@gmail.com>
2021-08-10 15:56:37 -0400

    Updates

---
+++ precache-manifest.2265a5a6d4439490ab1725c0e4887c22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74553749917c0bdadad5995dfbad47c2",
+    "url": "/insta-firebase/index.html"
+  },
+  {
+    "revision": "7ec8187de188f07d60e8",
+    "url": "/insta-firebase/static/css/main.e3bd3a6e.chunk.css"
+  },
+  {
+    "revision": "36ad720abd8dea9c4775",
+    "url": "/insta-firebase/static/js/2.522f3122.chunk.js"
+  },
+  {
+    "revision": "f13b8ee62297cdeb55e53ad5d3f0bd7f",
+    "url": "/insta-firebase/static/js/2.522f3122.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ec8187de188f07d60e8",
+    "url": "/insta-firebase/static/js/main.d272395d.chunk.js"
+  },
+  {
+    "revision": "6bc3961d204fb0664662",
+    "url": "/insta-firebase/static/js/runtime-main.96baebb0.js"
+  }
+]);
